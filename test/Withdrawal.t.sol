bbe372a96b8a8a7372165293eb231fc6999c5306
Alexander Biryukov <alexander.biryukov@p2p.org>
2022-11-01 10:26:34 +0300

    call from multisig

--- test/Withdrawal.t.sol
+++ test/Withdrawal.t.sol
@@ -8,9 +8,10 @@ import "forge-std/Vm.sol";
 import "forge-std/console.sol";
 import "forge-std/console2.sol";
 import "../src/ERC20_Vesting.sol";
+import "../src/IMultiSigWalletWithDailyLimit.sol";
 
 contract Withdrawal is Test {
-    address constant public multisigWallet = 0x592e720974D478832053AF79151f3482b1538F36;
+    address constant public multisigWallet_Address = 0x592e720974D478832053AF79151f3482b1538F36;
     address constant public ERC20_Vesting_Address = 0x23d1bFE8fA50a167816fBD79D7932577c06011f4;
     address constant public v2_address = 0xcB84d72e61e383767C4DFEb2d8ff7f4FB89abc6e;
 
@@ -21,22 +22,48 @@ contract Withdrawal is Test {
 
     ERC20_Vesting public erc20_Vesting;
     IERC20 public vegaToken;
+    IMultiSigWalletWithDailyLimit public multisig;
 
     function setUp() public {
         erc20_Vesting = ERC20_Vesting(ERC20_Vesting_Address);
         vegaToken = IERC20(v2_address);
+        multisig = IMultiSigWalletWithDailyLimit(multisigWallet_Address);
     }
 
     function testWithdrawal() public {
-        cheats.startPrank(multisigWallet);
         //vm.recordLogs();
 
-        uint256 vegaBalanceOfMultisigWalletBefore = vegaToken.balanceOf(multisigWallet);
+        uint256 vegaBalanceOfMultisigWalletBefore = vegaToken.balanceOf(multisigWallet_Address);
 
-        erc20_Vesting.withdraw_from_tranche(3);
-        erc20_Vesting.withdraw_from_tranche(8);
+//        uint256 tranche_3_balance = erc20_Vesting.get_tranche_balance(multisigWallet_Address, 3);
+//        emit log_uint(tranche_3_balance);
+//
+//        uint256 tranche_8_balance = erc20_Vesting.get_tranche_balance(multisigWallet_Address, 8);
+//        emit log_uint(tranche_8_balance);
+//
+//        uint256 vested_for_tranche_3 = erc20_Vesting.get_vested_for_tranche(multisigWallet_Address, 3);
+//        emit log_uint(vested_for_tranche_3);
+//
+//        uint256 vested_for_tranche_8 = erc20_Vesting.get_vested_for_tranche(multisigWallet_Address, 8);
+//        emit log_uint(vested_for_tranche_8);
 
-        uint256 vegaBalanceOfMultisigWalletAfter = vegaToken.balanceOf(multisigWallet);
+        bytes memory data3 = abi.encodeWithSelector(erc20_Vesting.withdraw_from_tranche.selector, uint8(3));
+
+        uint txCount = multisig.getTransactionCount(true, true);
+
+        cheats.startPrank(account1);
+        multisig.submitTransaction(ERC20_Vesting_Address, 0, data3);
+        cheats.stopPrank();
+
+        cheats.startPrank(account2);
+        multisig.confirmTransaction(txCount);
+        cheats.stopPrank();
+
+
+//        erc20_Vesting.withdraw_from_tranche(3);
+//        erc20_Vesting.withdraw_from_tranche(8);
+
+        uint256 vegaBalanceOfMultisigWalletAfter = vegaToken.balanceOf(multisigWallet_Address);
 
         emit log_uint(vegaBalanceOfMultisigWalletAfter - vegaBalanceOfMultisigWalletBefore);
 
