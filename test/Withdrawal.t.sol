edc1b2ec3fa09903a5ed3e25d185b1de6ed8efa8
Alexander Biryukov <alexander.biryukov@p2p.org>
2024-04-13 04:00:14 +0400

    new

--- test/Withdrawal.t.sol
+++ test/Withdrawal.t.sol
@@ -18,7 +18,7 @@ contract Withdrawal is Test {
     address constant public account1 = 0xF5EFD4E7ebe4203d3Ee592AA058809d0Ae613aCA;
     address constant public account2 = 0xaeF7d07F5aDC3084D43963009CCD67C6C68aea64;
 
-    address constant public anotherWallet = 0xdeAD00000000000000000000000000000000dEAd;
+    address constant public anotherWallet = 0x16da609341ed67750A8BCC5AAa2005471006Cd77;
 
     bytes32 constant public vega_public_key = hex"7A542A71397AEEFB4CBEAF5CDD565D475CBC87C63EB469C95497A5A6C2874797";
 
@@ -29,6 +29,8 @@ contract Withdrawal is Test {
     IMultiSigWalletWithDailyLimit public multisig;
 
     function setUp() public {
+        vm.createSelectFork("mainnet", 19642729);
+
         erc20_Vesting = ERC20_Vesting(ERC20_Vesting_Address);
         vegaToken = IERC20(v2_address);
         multisig = IMultiSigWalletWithDailyLimit(multisigWallet_Address);
@@ -38,33 +40,36 @@ contract Withdrawal is Test {
         uint256 vegaBalanceOfAnotherWalletBefore = vegaToken.balanceOf(anotherWallet);
 
         uint256 vested_for_tranche_3 = erc20_Vesting.get_vested_for_tranche(multisigWallet_Address, 3);
-        uint256 vested_for_tranche_8 = erc20_Vesting.get_vested_for_tranche(multisigWallet_Address, 8);
+        emit log_uint(vested_for_tranche_3);
 
-        uint256 totalVested = vested_for_tranche_3 + vested_for_tranche_8;
+//        uint256 vested_for_tranche_8 = erc20_Vesting.get_vested_for_tranche(multisigWallet_Address, 8);
+//        emit log_uint(vested_for_tranche_8);
+
+        uint256 totalVested = vested_for_tranche_3 + 20312000000000000000000 - 96698891828403708997217; // 1.840708639095229e22 + 1; // vested_for_tranche_3 + vested_for_tranche_8;
 
         bytes memory remove_stake = abi.encodeWithSelector(erc20_Vesting.remove_stake.selector, totalVested, vega_public_key);
         emit log_bytes(remove_stake);
         bytes memory withdraw_from_tranch_3 = abi.encodeWithSelector(erc20_Vesting.withdraw_from_tranche.selector, uint8(3));
         emit log_bytes(withdraw_from_tranch_3);
-        bytes memory withdraw_from_tranch_8 = abi.encodeWithSelector(erc20_Vesting.withdraw_from_tranche.selector, uint8(8));
-        emit log_bytes(withdraw_from_tranch_8);
-        bytes memory transfer_vega_to_another_wallet = abi.encodeWithSelector(vegaToken.transfer.selector, anotherWallet, totalVested);
+//        bytes memory withdraw_from_tranch_8 = abi.encodeWithSelector(erc20_Vesting.withdraw_from_tranche.selector, uint8(8));
+//        emit log_bytes(withdraw_from_tranch_8);
+        bytes memory transfer_vega_to_another_wallet = abi.encodeWithSelector(vegaToken.transfer.selector, anotherWallet, vested_for_tranche_3);
         emit log_bytes(transfer_vega_to_another_wallet);
 
         uint txCount = multisig.getTransactionCount(true, true);
 
-        cheats.startPrank(account2);
+        cheats.startPrank(account1);
         multisig.submitTransaction(ERC20_Vesting_Address, 0, remove_stake);
         multisig.submitTransaction(ERC20_Vesting_Address, 0, withdraw_from_tranch_3);
-        multisig.submitTransaction(ERC20_Vesting_Address, 0, withdraw_from_tranch_8);
-        multisig.submitTransaction(v2_address, 0, transfer_vega_to_another_wallet);
+        // multisig.submitTransaction(ERC20_Vesting_Address, 0, withdraw_from_tranch_8);
+        // multisig.submitTransaction(v2_address, 0, transfer_vega_to_another_wallet);
         cheats.stopPrank();
 
-        cheats.startPrank(account1);
+        cheats.startPrank(account2);
         multisig.confirmTransaction(txCount);
         multisig.confirmTransaction(txCount + 1);
-        multisig.confirmTransaction(txCount + 2);
-        multisig.confirmTransaction(txCount + 3);
+        // multisig.confirmTransaction(txCount + 2);
+        // multisig.confirmTransaction(txCount + 3);
         cheats.stopPrank();
 
         uint256 vegaBalanceOfAnotherWalletAfter = vegaToken.balanceOf(anotherWallet);
